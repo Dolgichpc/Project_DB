b3f0a70ec388bd801cb17540b41374454c8d2fda
Dolgichpc <71508660+Dolgichpc@users.noreply.github.com>
2020-09-20 19:08:09 +0900

    Add files via upload

--- 3.sql
+++ 3.sql
@@ -1,58 +1,68 @@
+/*3*/
+
 drop schema if exists project cascade;
 
 create schema if not exists project;
 SET search_path = project, public;
 
 create table Car (
- Nomber_cd varchar(13),
- Carrier_id integer,
+ Nomber_cd varchar(13) not null,
+ Carrier_id integer not null ,
  Brand_nm varchar(50),
- Places_amt integer
+ Places_amt integer,
+ primary key (Nomber_cd)
 ) ;
 create table Carrier (
- Carrier_id integer,
+ Carrier_id integer not null,
  Name_nm varchar(100),
  Passport_cnt varchar(250),
- INN_cd integer,
- SNILS_cd integer
+ INN_cd varchar(20),
+ SNILS_cd varchar(20),
+ primary key (Carrier_id)
 ) ;
 
 create table Contract (
-Сontract_id integer,
-Carrier_id varchar(100),
-Route_no integer,
+Сontract_id integer  not null,
+Carrier_id integer  not null,
+Route_no integer  not null,
 Nomber_cd varchar(20),
 First_date_dt date,
-Last_date_dt date
+Last_date_dt date,
+primary key (Сontract_id)
 ) ;
 
 create table Route (
-Route_no integer,
-Length_amt decimal(4,2)
+Route_no integer not null,
+Length_amt decimal(4,2),
+primary key (Route_no)
 ) ;
 
 create table Car_archive (
- Nomber_cd varchar(13),
- Carrier_id integer,
+ Nomber_cd varchar(13) not null,
+ Carrier_id integer not null ,
  Brand_nm varchar(50),
- Places_amt integer
+ Places_amt integer,
+ primary key (Nomber_cd)
 ) ;
 create table Carrier_archive (
- Carrier_id integer,
+Carrier_id integer not null,
  Name_nm varchar(100),
  Passport_cnt varchar(250),
- INN_cd integer,
- SNILS_cd integer
+ INN_cd varchar(20),
+ SNILS_cd varchar(20),
+ primary key (Carrier_id)
 ) ;
 create table Contract_archive (
-Сontract_id integer,
-Carrier_id varchar(100),
-Route_no integer,
+Сontract_id integer  not null,
+Carrier_id integer  not null,
+Route_no integer  not null,
 Nomber_cd varchar(20),
 First_date_dt date,
-Last_date_dt date
+Last_date_dt date,
+primary key (Сontract_id)
 ) ;
 create table Route_archive (
-Route_no integer,
-Length_amt decimal(4,2)
+Route_no integer not null,
+Length_amt decimal(4,2),
+primary key (Route_no)
 ) ;
